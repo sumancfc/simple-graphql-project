41efc28ae2ab32b78562cefb17edc9d11573fc27
Suman Shrestha <sumanstha999@gmail.com>
2021-09-21 15:30:44 +0545

    GraphQL project setup

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
+
+const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    graphiql: true,
+  })
+);
+
+app.listen(5000, () => console.log(`Server running on port 5000`));
